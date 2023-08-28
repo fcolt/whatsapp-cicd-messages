60b665064b8a65efde4b4874dbeaa7e2c4dd0242
Chocobruh <olteanufabian99@gmail.com>
2023-08-28 19:12:57 +0300

    test

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const groupName = process.env.GROUP_NAME;
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
