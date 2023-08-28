0fe4ac89682d32697e81b9d105c530e5708838a6
Chocobruh <olteanufabian99@gmail.com>
2023-08-28 22:39:43 +0300

    v1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const qrcode = require("qrcode-terminal");
+const qrcode = require("qrcode");
 const chromium = require("@sparticuz/chromium-min");
 const { Client, LocalAuth } = require("whatsapp-web.js");
-const app = require('express')();
+const app = require("express")();
+require('dotenv').config();
 
 const groupName = process.env.GROUP_NAME;
 
@@ -10,17 +11,29 @@ const getClient = async () =>
     puppeteer: {
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath('https://github.com/stefanjudis/tiny-helpers/raw/primary/static/chromium/chromium-pack.tar'),
+      executablePath: await chromium.executablePath(
+        "https://github.com/stefanjudis/tiny-helpers/raw/primary/static/chromium/chromium-pack.tar"
+      ),
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     },
   });
 
-getClient().then((client) => {
-  client.on("qr", (qr) => {
-    qrcode.generate(qr, { small: true }, function(qr) {
-      console.info(qr);
-    });
+app.get("/", async (req, res) => {
+  const client = await getClient();
+
+  client.on("qr", async (qr) => {
+    try {
+      const qrImageBuffer = await qrcode.toBuffer(qr);
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': qrImageBuffer.length,
+      });
+      res.end(qrImageBuffer);
+    } catch (error) {
+      console.error("Error generating QR code:", error);
+      res.status(500).send("Internal Server Error");
+    }
   });
 
   client.on("ready", () => {
@@ -42,7 +55,7 @@ getClient().then((client) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server started');
+  console.log("Server started");
 });
 
 module.exports = app;
