8941abd3ed6a7d2bde2c02596470e2f63df71538
Chocobruh <olteanufabian99@gmail.com>
2023-08-28 19:40:26 +0300

    testing with chrome-aws-lambda

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const qrcode = require("qrcode-terminal");
+const chromium = require("chrome-aws-lambda");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const groupName = process.env.GROUP_NAME;
 
-const client = new Client({
-  puppeteer: {
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  },
-});
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("ready", () => {
-  console.log("Initialized");
-});
-
-client.on("message", (message) => {
-  if (!message.from === process.env.FROM_ID) {
-    return;
-  }
-
-  client.getChats().then((chats) => {
-    const myGroup = chats.find((chat) => chat.name === groupName);
-    client.sendMessage(myGroup.id._serialized, message.body);
+const getClient = async () =>
+  new Client({
+    puppeteer: {
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    },
   });
-});
+;
+getClient().then((client) => {
+  client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
 
-client.initialize();
+  client.on("ready", () => {
+    console.log("Initialized");
+  });
+
+  client.on("message", (message) => {
+    if (!message.from === process.env.FROM_ID) {
+      return;
+    }
+
+    client.getChats().then((chats) => {
+      const myGroup = chats.find((chat) => chat.name === groupName);
+      client.sendMessage(myGroup.id._serialized, message.body);
+    });
+  });
+
+  client.initialize();
+});
