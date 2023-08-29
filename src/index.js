f5a4c250bd27527f365ec7cfa80597f629a5a0d3
Chocobruh <olteanufabian99@gmail.com>
2023-08-29 11:32:39 +0300

    test with mongodb

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 const qrcode = require("qrcode");
 const chromium = require("@sparticuz/chromium-min");
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, RemoteAuth } = require("whatsapp-web.js");
 const app = require("express")();
-require('dotenv').config();
+require("dotenv").config();
+const { MongoStore } = require("wwebjs-mongo");
+const mongoose = require("mongoose");
 
 const groupName = process.env.GROUP_NAME;
 
-const getClient = async () =>
-  new Client({
-    authStrategy: new LocalAuth({
-      dataPath: './.wwebjs_auth/'
+const getClient = async () => {
+  // const chromiumExecutablePath = await chromium.executablePath(
+  //   "https://github.com/stefanjudis/tiny-helpers/raw/primary/static/chromium/chromium-pack.tar"
+  // );
+  await mongoose.connect(process.env.MONGODB_URI);
+  const store = new MongoStore({ mongoose: mongoose });
+  const client = new Client({
+    authStrategy: new RemoteAuth({
+      store: store,
+      backupSyncIntervalMs: 300000,
     }),
     puppeteer: {
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        "https://github.com/stefanjudis/tiny-helpers/raw/primary/static/chromium/chromium-pack.tar"
-      ),
+      // executablePath: chromiumExecutablePath,
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     },
   });
+
+  return client;
+};
 
 app.get("/", async (req, res) => {
   const client = await getClient();
@@ -29,8 +38,8 @@ app.get("/", async (req, res) => {
     try {
       const qrImageBuffer = await qrcode.toBuffer(qr);
       res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': qrImageBuffer.length,
+        "Content-Type": "image/png",
+        "Content-Length": qrImageBuffer.length,
       });
       res.end(qrImageBuffer);
     } catch (error) {
