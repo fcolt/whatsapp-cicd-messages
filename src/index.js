748fa69c0e8f0f7511a860549065b0ab9b7b22b5
Chocobruh <olteanufabian99@gmail.com>
2023-09-03 22:59:50 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,65 +1,29 @@
-const qrcode = require("qrcode");
-const chromium = require("@sparticuz/chromium-min");
+const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
-const app = require("express")();
-require('dotenv').config();
 
 const GROUP_NAME = process.env.GROUP_NAME;
-const PORT = process.env.PORT;
 
-const getClient = async () =>
-  new Client({
-    authStrategy: new LocalAuth(/*{
-      dataPath: '/tmp/.wwebjs_auth'
-    }*/),
-    /*puppeteer: {
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        "https://github.com/stefanjudis/tiny-helpers/raw/primary/static/chromium/chromium-pack.tar"
-      ),
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    },*/
-  });
-
-app.get("/", async (req, res) => {
-  const client = await getClient();
-
-  client.on("qr", async (qr) => {
-    try {
-      const qrImageBuffer = await qrcode.toBuffer(qr);
-      res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': qrImageBuffer.length,
-      });
-      res.end(qrImageBuffer);
-    } catch (error) {
-      console.error("Error generating QR code:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
-
-  client.on("ready", () => {
-    console.log("Initialized");
-  });
-
-  client.on("message", (message) => {
-    if (!message.from === process.env.FROM_ID) {
-      return;
-    }
-
-    client.getChats().then((chats) => {
-      const myGroup = chats.find((chat) => chat.name === GROUP_NAME);
-      client.sendMessage(myGroup.id._serialized, message.body);
-    });
-  });
-
-  client.initialize();
+const client = new Client({
+  authStrategy: new LocalAuth(),
 });
 
-app.listen(PORT, () => {
-  console.log("Server started");
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-module.exports = app;
+client.on("ready", () => {
+  console.log("Initialized");
+});
+
+client.on("message", (message) => {
+  if(!message.from === process.env.FROM_ID) {
+    return;
+  }
+
+  client.getChats().then((chats) => {
+    const myGroup = chats.find((chat) => chat.name === GROUP_NAME);
+    client.sendMessage(myGroup.id._serialized, message.body);
+  });
+});
+
+client.initialize();
