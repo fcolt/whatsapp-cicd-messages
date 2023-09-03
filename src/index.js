96af808a4f2649ca419cd6c849529b2b42df14bf
Chocobruh <olteanufabian99@gmail.com>
2023-09-03 23:34:35 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,51 +1,57 @@
 const qrcode = require("qrcode");
-const chromium = require("@sparticuz/chromium-min");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const app = require("express")();
-require('dotenv').config();
+require("dotenv").config();
 
 const GROUP_NAME = process.env.GROUP_NAME;
 const PORT = process.env.PORT;
+let qrCode;
 
-const getClient = async () =>
-  new Client({
-    authStrategy: new LocalAuth()
-  });
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
 
 app.get("/", async (req, res) => {
-  const client = await getClient();
-
-  client.on("qr", async (qr) => {
-    try {
-      const qrImageBuffer = await qrcode.toBuffer(qr);
-      res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': qrImageBuffer.length,
-      });
-      res.end(qrImageBuffer);
-    } catch (error) {
-      console.error("Error generating QR code:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
-
-  client.on("ready", () => {
-    console.log("Initialized");
-  });
-
-  client.on("message", (message) => {
-    if (!message.from === process.env.FROM_ID) {
-      return;
-    }
-
-    client.getChats().then((chats) => {
-      const myGroup = chats.find((chat) => chat.name === GROUP_NAME);
-      client.sendMessage(myGroup.id._serialized, message.body);
+  if (!qrCode) {
+    return res.status(204).json({
+      status: "success",
+      data: [],
+      message: 'No qr code yet generated',
     });
-  });
-
-  client.initialize();
+  }
+  try {
+    const qrImageBuffer = await qrcode.toBuffer(qrCode);
+    res.writeHead(200, {
+      "Content-Type": "image/png",
+      "Content-Length": qrImageBuffer.length,
+    });
+    res.end(qrImageBuffer);
+  } catch (error) {
+    console.error("Error generating QR code:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+client.on("qr", async (qr) => {
+  qrCode = qr;
+});
+
+client.on("ready", () => {
+  console.log("Initialized");
+});
+
+client.on("message", (message) => {
+  if (!message.from === process.env.FROM_ID) {
+    return;
+  }
+
+  client.getChats().then((chats) => {
+    const myGroup = chats.find((chat) => chat.name === GROUP_NAME);
+    client.sendMessage(myGroup.id._serialized, message.body);
+  });
+});
+
+client.initialize();
 
 app.listen(PORT, () => {
   console.log("Server started");
