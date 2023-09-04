14f724c5e16b14633baea548d617aae2dfe27ef3
Chocobruh <olteanufabian99@gmail.com>
2023-09-04 22:20:44 +0300

    auto reopening of browser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const qrcode = require("qrcode");
-const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const app = require("express")();
+const puppeteer = require('puppeteer');
 require("dotenv").config();
 
 const GROUP_NAME = process.env.GROUP_NAME;
 const PORT = process.env.PORT;
 
+class MyLocalAuth extends LocalAuth {
+  constructor(opts) {
+    super(opts);
+  }
+  async afterBrowserInitialized() {
+    super.afterBrowserInitialized();
+    this.client.pupBrowser.on("disconnected", () =>
+      this.client.emit("pup_disconnected")
+    );
+  }
+}
+
 const client = new Client({
+  authStrategy: new MyLocalAuth(),
   restartOnAuthFail: true,
   puppeteer: {
     headless: true,
@@ -20,8 +34,8 @@ const client = new Client({
       "--deterministic-fetch",
       "--disable-features=IsolateOrigins",
       "--disable-site-isolation-trials",
-    ]
-  }
+    ],
+  },
 });
 
 app.get("/", async (req, res) => {
@@ -51,8 +65,22 @@ client.on("ready", () => {
   setInterval(() => client.pupPage.click("#pane-side"), 60 * 1000); //"click" window in one minute intervals so that the session doesn't close
   setInterval(
     () => client.sendMessage(process.env.FROM_ID, "."),
-    1000 * 60 * 60 * 24 - 2000 * 60
+    1000 * 60 * 60 * 24 - 10000 * 60
   ); //send a message to the whatsapp bot every day to receive its messages
+});
+
+const launchBrowser = async () => {
+  client.pupBrowser = await puppeteer.launch();
+  let page = await client.pupBrowser.newPage();
+  await page.goto('https://whatsapp.com/');
+  console.log('Browser reconnected.');
+}
+
+client.on("pup_disconnected", () => {
+  console.log('Browser disconnected, attempting to reconnect');
+  (async () => {
+    await launchBrowser();
+  })();
 });
 
 client.on("message", (message) => {
